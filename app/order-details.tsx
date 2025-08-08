752b1ecd70d16168db54a59950d0b151121bcfe0
Ruslan Vyshnevskyi <56252678+tess-4aek@users.noreply.github.com>
2025-08-08 14:56:55 +0300

    Update back button design to match other pages

--- app/order-details.tsx
+++ app/order-details.tsx
@@ -285,6 +285,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.08,
     shadowRadius: 8,
     elevation: 3,
+    shadowColor: '#0C1E3C',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.08,
+    shadowRadius: 8,
+    elevation: 3,
   },
   headerTitle: {
     fontSize: 18,
