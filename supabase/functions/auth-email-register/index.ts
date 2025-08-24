c527ad1e24df947b8391e3b0a718e5639fee02db
Ruslan Vyshnevskyi <56252678+tess-4aek@users.noreply.github.com>
2025-08-25 01:06:52 +0300

    Updated index.ts

--- supabase/functions/auth-email-register/index.ts
+++ supabase/functions/auth-email-register/index.ts
@@ -65,8 +65,7 @@ function withCors(handler: (req: Request) => Promise<Response>) {
   };
 }
 
-const corsHeaders = getCorsHeaders();
-
+const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, OPTIONS'
