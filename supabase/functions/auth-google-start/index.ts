aa16adc4be970dfbd6aeb7f6a1c363a92c2ed660
Ruslan Vyshnevskyi <56252678+tess-4aek@users.noreply.github.com>
2025-08-25 01:06:36 +0300

    Updated index.ts

--- supabase/functions/auth-google-start/index.ts
+++ supabase/functions/auth-google-start/index.ts
@@ -45,8 +45,7 @@ function withCors(handler: (req: Request) => Promise<Response>) {
   };
 }
 
-const corsHeaders = getCorsHeaders();
-
+const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
