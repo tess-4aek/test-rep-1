cf54959698e39ec3c7ed72520fe8e46849c63ae6
Ruslan Vyshnevskyi <56252678+tess-4aek@users.noreply.github.com>
2025-09-03 14:31:38 +0300

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, User, History } from 'lucide-react-native';
+import { Chrome as Home, User, History } from 'lucide-react-native';
 import { t } from '@/lib/i18n';
 
 export default function TabLayout() {
